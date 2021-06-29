65dccb4f2854d0ab79fd3254cb5fd6f36ab3ea44
ArturM <amrozinski@gmail.com>
2021-06-29 09:08:22 +0100

    second commit

---
+++ railsvue-front/src/components/Contacts.vue
@@ -0,0 +1,31 @@
+<template>
+ <div id="contacts">
+  <p>aaa</p>
+  <div v-for="(contact, index) in contacts" :key="contact.id">
+   <p>{{contact.name}} is my {{contact.relationship}}</p>
+  </div>
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+ data() {
+  return {
+   contacts: [],
+   name: [],
+   relationship: [],
+   errors: []
+  }
+ },
+ mounted() {
+  axios.get('http://localhost:3000/contacts')
+  .then(response => {
+   this.contacts = response.data
+  })
+ .catch(e => {
+  this.error.push(e)
+  })
+ },
+}
+</script>
